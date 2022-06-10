a4680385de08bd41c6d04d38040608c86c5ac029
Jaseela99 <jaseelachittur@gmail.com>
2022-06-10 14:08:22 +0530

    completed tictac toe

--- tac.js
+++ tac.js
@@ -1,143 +1,158 @@
-//div for player 
-const playerDiv = document.querySelector(".player")
-const restartDiv = document.querySelector(".restart")
-//each squares is manipulated by squareDiv
-const squareDivs = document.querySelector(".square")
+window.addEventListener("DOMContentLoaded",()=>{
+    //queryselectorall returns an array like objects to convert it in to proper array we use Array.from()
+    const squares = Array.from(document.querySelectorAll(".square"))
+    //display player
+    const player = document.querySelector(".display-player")
+    //reset game
+    const resetButton = document.querySelector("#reset")
+    //to announce the winner
+    const result = document.querySelector(".result")
 
-//players
+    //board with 9 empty values
+    let board = ['','','','','','','','','']
+    let currentPlayer ="X"//x or o
+    let isGameOn =true; //game is on
 
-const firstPlayer ="X"
-const secondPlayer ="O"
+    //end game
+    const PLAYERX_WON ="PLAYERX_WON"
+    const PLAYERO_WON ="PLAYERO_WON"
+    const TIE = "TIE"
 
-//booleans for game
-let isGame = true
-let isXNext =true
-//determines who plays the game
-const whoPlays =(play)=> play==="X" ? firstPlayer:secondPlayer
+    /*
+        Indexes within the board
+        [0] [1] [2]
+        [3] [4] [5]
+        [6] [7] [8]
+    */
+
+        //game will win
+        const winningConditions =[
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+        function handleResultValidation() {
+            //game won
+            let roundWon = false;
+            //looping through winning conditions
+            for (let i = 0; i <= 7; i++) {
+                const winCondition = winningConditions[i];
+                const a = board[winCondition[0]];
+                const b = board[winCondition[1]];
+                const c = board[winCondition[2]];
+                //if any of squares in the board has empty strings then loop continues
+                if (a === '' || b === '' || c === '') {
+                    continue;
+                }
+                //if a b and c are equal then game is won loop is broke
+                if (a === b && b === c) {
+                    roundWon = true;
+                    break;
+                }
+            }
+           //if game won and current player is X then type will be PLAYERX_WON and game will be inactive
+            if (roundWon) {
+                announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
+                isGameOn = false;
+                return;
+            }
+        //if  board doesnt have any empty strings type become tie
+        if (!board.includes(''))
+            announce(TIE);
+        }
+    //result winner is displayed else tie 
+        const announce = (type) => {
+            switch(type){
+                case PLAYERO_WON:
+                    result.innerHTML = 'Player <span class="playerO">O</span> Won';
+                    break;
+                case PLAYERX_WON:
+                    result.innerHTML = 'Player <span class="playerX">X</span> Won';
+                    break;
+                case TIE:
+                    result.innerText = 'Tie';
+            }
+            //removes hide class name from result clss hence it becomes visible
+            result.classList.remove('hide');
+        };
+       //to decide whethre the square has x or o
+        const isValidAction = (square) => {
+            //if square has x or O returns false else true
+            if (square.innerText === 'X' || square.innerText === 'O'){
+                return false;
+            }
+            return true;
+        };
+       //board[index] is relaced with current players letter
+        const updateBoard =  (index) => {
+            board[index] = currentPlayer;
+        }
+      //player changing alternatively
+        const changePlayer = () => {
+            //removes the current player classname 
+            player.classList.remove(`player${currentPlayer}`); //x
+            //if current player is x replaced by o
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';//o
+            //also the player
+            player.innerText = currentPlayer; //"o"
+            //classname of playerO is added
+            player.classList.add(`player${currentPlayer}`); //playerO
+        }
+    
+        const userAction = (square, index) => {
+            //if the board has empty spaces and game is on then 
+            if(isValidAction(square) && isGameOn) {
+                //square is changed from "" to currentplayer
+                square.innerText = currentPlayer;
+                //player letter clssname is added to the square
+                square.classList.add(`player${currentPlayer}`);
+                //board is updated
+                updateBoard(index);
+                //checks the winning condition 
+                handleResultValidation();
+                //changes the player
+                changePlayer();
+            }
+        }
+        //board and game is active 
+        const resetBoard = () => {
+            board = ['', '', '', '', '', '', '', '', ''];
+            isGameOn = true;
+            //hide is added to result
+            result.classList.add('hide');
+    //initial player is given to x
+            if (currentPlayer === 'O') {
+                changePlayer();
+            }
+            squares.forEach(square => {
+                //each square is given as empty string 
+                square.innerText = '';
+                //player classname is removed from each square
+                square.classList.remove('playerX');
+                square.classList.remove('playerO');
+            });
+        }
+       //for each click on the square player letter is given for the particular index
+        squares.forEach( (square, index) => {
+            square.addEventListener('click', () => userAction(square, index));
+        });
+//resetboard executes when reseyt button is clicked
+    resetButton.addEventListener("click",resetBoard)
+})
 
 
-//who wins
-const winGame =(play)=>{
-    //game is not played
-    isGame=false
-    //player who win is displayed
-    playerDiv.innerHTML = `<span>${whoPlays(play)}  won...</span>`
-
-}
-
-//gameStatus
-
-const gameStatus =()=>{
-    const square1 = squareDivs[0].classList[1];
-    const square2 = squareDivs[1].classList[1];
-    const square3 = squareDivs[2].classList[1];
-    const square4 = squareDivs[3].classList[1];            // defines each square 
-    const square5 = squareDivs[4].classList[1];
-    const square6 = squareDivs[5].classList[1];
-    const square7 = squareDivs[6].classList[1];
-    const square8 = squareDivs[7].classList[1];
-    const square9 = squareDivs[8].classList[1];
-
-//checks in columns
-
-if(square1 && square1 === square2 && square1===square3){
-    winGame(square1)
-    squareDivs[0].classList.add("won")
-    squareDivs[1].classList.add("won")
-    squareDivs[2].classList.add("won")
-}else if(square4 && square4 === square5 && square4 === square6 ){
-    winGame(square3)
-    squareDivs[3].classList.add("won")
-    squareDivs[4].classList.add("won")
-    squareDivs[5].classList.add("won")
-}else if(square7 && square7 === square8 && square7 === square9 ){
-    winGame(square7)
-    squareDivs[6].classList.add("won")
-    squareDivs[7].classList.add("won")
-    squareDivs[8].classList.add("won")
-}
-//check in rows
-else if(square1  && square1 === square4 && square1 === square7 ){
-    winGame(square1)
-    squareDivs[0].classList.add("won")
-    squareDivs[3].classList.add("won")
-    squareDivs[6].classList.add("won")
-}
-else if(square2  && square2 === square5 && square2 === square8 ){
-    winGame(square2)
-    squareDivs[1].classList.add("won")
-    squareDivs[4].classList.add("won")
-    squareDivs[7].classList.add("won")
-}
-else if(square3  && square3 === square6 && square3 === square9 ){
-    winGame(square3)
-    squareDivs[2].classList.add("won")
-    squareDivs[5].classList.add("won")
-    squareDivs[8].classList.add("won")
-}
-//diagonals
-else if(square1 && square1===square5 && square1===square9){   // checks diagonally
-    GameWin(square1);
-    squareDivs[0].classList.add('won');
-    squareDivs[4].classList.add('won');
-    squareDivs[8].classList.add('won');
-
-}else if(square3 && square3===square5 && square3===square7){    // chekcks diagonally
-    GameWin(square3);   
-    squareDivs[2].classList.add('won');
-    squareDivs[4].classList.add('won');
-    squareDivs[6].classList.add('won');
-
-}
-//tie
-
-else if (square1 && square2 && square3 && square4 && square5 && square6 && square7 && square8 && square9){
-    isGame = false
-    playerDiv.innerHTML ="It's a Tie"
-}else{
-    //alternatively change the player turn
-    isXNext=!isXNext
-    //determining whose turn it is
-    isXNext ? playerDiv.innerHTML = `${firstPlayer}'s turn`: playerDiv.innerHTML =`${secondPlayer}'s turn`
-}
-
-const restartCheck =()=>{
-    //for next game x should start
-    isXNext=true
-    playerDiv.innerHTML =`${firstPlayer}'s turn`
-    //to remove x and o s from squares 
-    for(const squareDiv of squareDivs){
-        squareDiv.classList.remove("X")
-        squareDiv.classList.remove("O")
-        squareDiv.classList.remove("won")
-    }
-    //game is on
-    isGame = true
-}
-
-//to add x and o in game box
-const squareClick =(e)=>{
-    const classList = e.target.classList
-
-    if(!isGame || classList[1]==='X' || classList[1]==='O'){
-        return;
-    }
-    if(isXNext){
-        classList.add('X');
-        gameStatus();
-    }else{
-        classList.add('O');
-        gameStatus();
-    }
-}
-//event listeners for restart and squareclick
-
-restartDiv.addEventListener("click",restartCheck) 
-
-for(const squareDiv of squareDivs){
-    squareDiv.addEventListener("click",squareClick)
-}
 
 
-}
+
+
+
+
+
+
+
 
